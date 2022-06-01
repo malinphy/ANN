fa48cafc3849b37f458025c84280917b9091d9ff
malinphy <55249305+malinphy@users.noreply.github.com>
2022-06-02 02:09:06 +0300

    Create scaNN.py

---
+++ scaNN/scaNN.py
@@ -0,0 +1,14 @@
+!pip install --quiet scann
+import scann
+import numpy as np
+data = np.random.rand(500,6)
+# print(data)
+query = np.random.rand(1,6)
+print(query)
+# use scann.scann_ops.build() to instead create a TensorFlow-compatible searcher
+searcher = scann.scann_ops_pybind.builder(data, 8, "dot_product").tree(
+    num_leaves=3, num_leaves_to_search=100, training_sample_size=250000).score_ah(
+    2, anisotropic_quantization_threshold=0.2).reorder(100).build()
+neighbors, distances = searcher.search_batched(query)
+print(neighbors)
+print(distances)
